7d2dbeb8206532388adc2f0ed78b81d9a29475d4
qjerome <qjerome@rawsec.lu>
2024-10-01 11:07:05 +0200

    chg: extract vmlinuz inird from ubuntu image

--- scripts/extract-initrd-vmlinuz.sh
+++ scripts/extract-initrd-vmlinuz.sh
@@ -1,4 +1,5 @@
 IMAGE="$(realpath $1)"
+OUT_DIR="$(dirname $IMAGE)"
 NBD=
 
 set -euxo pipefail
@@ -24,32 +25,58 @@ sleep 1
 
 while read line
 do
+    found=${found-0}
     tmp=$(mktemp -d)
     dev=$(awk '{print $1}'<<<$line)
     offset=$(awk '{print $2}'<<<$line)
 
+    # stop condition
+    if ls $OUT_DIR | grep -P 'vmlinuz' & ls $OUT_DIR | grep -P 'initr'
+    then
+        break;
+    fi
+
     # by default consider 512 size sectors
     if sudo mount -o ro,offset=$(($offset*512)) $NBD $tmp
     then
+
+        # debian stores it in a partition with /boot directory
         boot=$tmp/boot
         if [[ -d $boot ]]
         then
             ls -hail $boot
-            # we files needed to boot the vm
+            # we copy files needed to boot the vm
             while read file
             do
                 # we store vmlinuz and initrd in the directory
                 # where the image is located
-                tmp_file=$(mktemp --suffix="-extracted")
+                tmp_file=$(sudo mktemp --suffix="-extracted")
                 # some images require root to read file(s)
                 sudo cp $file $tmp_file
                 sudo chown $(id -u):$(id -g) $tmp_file
-                cp $tmp_file $(dirname $IMAGE)/$(basename $file)
+                cp $tmp_file $OUT_DIR/$(basename $file)
             done < <(find $boot -type f -name vmlinuz* -or -name initrd* -or -name initramfs*)
+
+        # ubuntu stores vmlinuz and initrd in a separate partition
+        elif ls $tmp | grep -P 'vmlinuz-' & ls $tmp | grep -P 'initrd.img-'
+        then
+            # we copy files needed to boot the vm
+            while read file
+            do
+                # we store vmlinuz and initrd in the directory
+                # where the image is located
+                tmp_file=$(sudo mktemp --suffix="-extracted")
+                # some images require root to read file(s)
+                sudo cp $file $tmp_file
+                sudo chown $(id -u):$(id -g) $tmp_file
+                cp $tmp_file $OUT_DIR/$(basename $file)
+            done < <(find $tmp -type f -name vmlinuz-* -or -name initrd.img-*)
         fi
+
         # unmount partition
         sudo umount $tmp
     fi
+
     rmdir $tmp
 done < <(sudo fdisk -l $NBD | awk '/^\/dev/{print $1, $2}')
 
