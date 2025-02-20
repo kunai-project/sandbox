d67ed9dcc233ed8ee4851fbaef37140bd25d5cb8
qjerome <qjerome@rawsec.lu>
2025-02-20 13:34:08 +0100

    enhance: initrd and vmlinuz extraction without root

--- scripts/extract-initrd-vmlinuz.sh
+++ scripts/extract-initrd-vmlinuz.sh
@@ -4,92 +4,43 @@ NBD=
 
 set -euxo pipefail
 
-for i in {0..10}
-do
-    if ! sudo fuser /dev/nbd$i
-    then
-        NBD=/dev/nbd$i
-        break;
-    fi
-done
-
 if [[ ! -f $IMAGE ]]
 then
     echo "Usage: $0 [VM_DISK_IMAGE]"
     exit 1
 fi
 
-# we connect a nbd
-sudo qemu-nbd -r -c $NBD $IMAGE
-sleep 1
+RAW=${IMAGE}.raw
 
-while read line
-do
-    found=${found-0}
-    tmp=$(mktemp -d)
-    dev=$(awk '{print $1}'<<<$line)
-    offset=$(awk '{print $2}'<<<$line)
+qemu-img convert -O raw $IMAGE $RAW
 
-    # stop condition
-    if ls $OUT_DIR | grep -P 'vmlinuz' & ls $OUT_DIR | grep -P 'initr'
-    then
-        break;
-    fi
-
-    # by default consider 512 size sectors
-    if sudo mount -o ro,offset=$(($offset*512)) $NBD $tmp
-    then
-
-        # debian stores it in a partition with /boot directory
-        boot=$tmp/boot
-        if [[ -d $boot ]]
-        then
-            ls -hail $boot
-            # we copy files needed to boot the vm
-            while read file
-            do
-                # we store vmlinuz and initrd in the directory
-                # where the image is located
-                tmp_file=$(sudo mktemp --suffix="-extracted")
-                # some images require root to read file(s)
-                sudo cp $file $tmp_file
-                sudo chown $(id -u):$(id -g) $tmp_file
-                cp $tmp_file $OUT_DIR/$(basename $file)
-            done < <(find $boot -type f -name vmlinuz* -or -name initrd* -or -name initramfs*)
-
-        # ubuntu stores vmlinuz and initrd in a separate partition
-        elif ls $tmp | grep -P 'vmlinuz-' & ls $tmp | grep -P 'initrd.img-'
-        then
-            # we copy files needed to boot the vm
-            while read file
-            do
-                # we store vmlinuz and initrd in the directory
-                # where the image is located
-                tmp_file=$(sudo mktemp --suffix="-extracted")
-                # some images require root to read file(s)
-                sudo cp $file $tmp_file
-                sudo chown $(id -u):$(id -g) $tmp_file
-                cp $tmp_file $OUT_DIR/$(basename $file)
-            done < <(find $tmp -type f -name vmlinuz-* -or -name initrd.img-*)
+function extract_files()
+{
+    container="$1"
+    while read file
+    do  
+        # if this is not a symbolic link
+        if 7z l -slt $container $file | grep 'Symbolic Link = $'; then
+            7z e -aoa -o./ $container $file
         fi
+    done < <(7z l -slt $container | grep '^Path = ' | cut -d '=' -f 2 | sed 's/^ //' | grep -E '(^(boot/)?)(vmlinuz|initrd|initramfs).*?')
+}
 
-        # unmount partition
-        sudo umount $tmp
+extract_files $RAW
+
+for i in {0..10}
+do
+    if 7z e -aoa -o./ $RAW $i.img > /dev/null; then
+        if [ -f  $i.img ]; then
+            echo "we extracted $i.img"
+            extract_files "$i.img"
+            rm $i.img
+        else
+            # no need to try extracting further
+            break;
+        fi
     fi
+done
 
-    rmdir $tmp
-done < <(sudo fdisk -l $NBD | awk '/^\/dev/{print $1, $2}')
-
-# we disconnect our nbd
-sudo qemu-nbd -d $NBD
-
-# vmlinux suffix
-SUFF=$(echo vmlinuz-* | cut -d '-' -f 2-)
-
-# sometimes there are several initrd files 
-# extracted and we need to remove useless ones
-if [[ -f "initrd.img-$SUFF" ]]
-then
-    rm initrd.img.old
-    rm initrd.img
-fi
+# we remove raw image we created
+rm $RAW
